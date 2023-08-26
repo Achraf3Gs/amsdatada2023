d107d7f3fc4e67caec1bdddd07b0b271f071e42d
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-26 13:08:53 +0200

    amsdata2023

---
+++ src/main/java/com/sip/ams/Amsdata2023Application.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amsdata2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amsdata2023Application.class, args);
+	}
+
+}
