29cec2d4ed0fb9c41bd5d4b34e81dc4aa4f980e3
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-27 21:35:08 +0200

    partie activation compte

--- src/main/java/com/sip/ams/service/UserService.java
+++ src/main/java/com/sip/ams/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
     public void saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(0);
-        Role userRole = roleRepository.findByRole("USER");
+        Role userRole = roleRepository.findByRole("ADMIN");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         userRepository.save(user);
     }
